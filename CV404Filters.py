3864daa9470203a0c444cedb8148503c0ad26386
bravewarrior0 <bravewarrior1997@gmail.com>
2020-03-05 05:39:43 +0200

    add covolve, map_img and add auto sigma to gaussian

--- CV404Filters.py
+++ CV404Filters.py
@@ -10,6 +10,33 @@ def rgb2gray(rgb_image):
     # convert RGB img to grayScale img
     return np.dot(rgb_image[...,:3], [0.299, 0.587, 0.114])
 
+def make_odd(num = 3):
+    num = num//2 *2 + 1
+    return num
+
+def convolve_img (img, kernal):
+    if len(img.shape) == 3:
+        out = np.zeros_like(img)
+        for i in range(3):
+            out[:,:,i]= signal.convolve2d(img[:,:,i],kernal,mode='same')
+        return out
+    elif len(img.shape) == 2:
+        return signal.convolve2d(img,kernal,mode='same')
+
+def img_map_gray(img):
+    if(img.min()<0):
+        img-=img.min()
+    img = img / img.max()
+    return img
+    
+def img_map(img):
+    if len(img.shape) == 3:
+        for i in range(3):
+            img[:,:,i]= img_map_gray(img[:,:,i])*255
+    elif len(img.shape) == 2:
+        img = img_map_gray(img)
+    return img
+
 def generate_gaussian_noise( mu, sigma, img_size ):
     #generrate random Gaussian noise array 
     return np.random.normal( mu, sigma, img_size)
@@ -20,18 +47,13 @@ def add_gaussian_noise(mu, sigma, img):
     img_with_gaussian_noise = img + gaussian_noise
     return img_with_gaussian_noise
 
-def gaussian_Filter(sigma = 0.1, shape= [3,3]):
+def gaussian_filter(shape = 3, sigma ='auto'):
     # generate gaussian kernal
-    shape = np.asarray(shape)
-    shape = shape//2 *2 + 1
+    shape = make_odd(shape)
     [m,n] = shape//2
     filter = np.zeros(shape)
-    # to set limts uncomment this
-    # size = 2*int(4*sigma + 0.5) + 1
-    # if shape[0] > size:
-    #     m = size//2
-    # if shape[1] > size:
-    #     n = size//2
+    if sigma is 'auto':
+        sigma = sigma=np.sqrt(2*m+1)
     for x in range (-m, m+1):
         for y in range (-m, m+1):
             filter[x+m, y+m] =np.exp(-(x**2 + y**2)/(2*sigma**2))/(2*np.pi*(sigma**2))
@@ -39,9 +61,9 @@ def gaussian_Filter(sigma = 0.1, shape= [3,3]):
     filter = filter * (1/sum)
     return filter
 
-def img_gaussian_Filter(img, sigma=0.1, shape=(3,3)):
-    kernal = gaussian_Filter(sigma,shape)
-    return signal.convolve2d(img,kernal,mode='valid')
+def img_gaussian_filter(img, shape= 3, sigma='auto'):
+    kernal = gaussian_filter(shape, sigma)
+    return convolve_img(img,kernal)
 
 def roberts_edge_detection(img):
     # output = np.sqrt(roberts_H_edge_detection(img)**2 + roberts_V_edge_detection(img)**2)
