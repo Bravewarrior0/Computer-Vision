10455b72ae2245b37d3542a0f2a61c937fe780e6
bravewarrior0 <bravewarrior1997@gmail.com>
2020-05-13 03:44:52 +0200

    modify readme

--- CV404Hough.py
+++ CV404Hough.py
@@ -63,97 +63,23 @@ def get_hough_lines(img, threshold = 0.3):
     gray = rgb2gray(img)
     edge = canny(gray)
     accs, ths, rs  = hough_line(edge)
-    fig, ax = plt.subplots()
-    ax.imshow(img)
+    
+    h,w = np.array(gray.shape)/100
+    fig = plt.figure(figsize=(w,h))
+    ax = fig.add_axes([0,0,1,1])
+
+    ax.imshow(img,cmap='gray')
     ax.autoscale(False)
     ax.axis('off')
     origin = np.array((0, img.shape[1]))
     for _, angle, dist in zip(*extract_lines(accs, ths, rs,threshold)):
         y0, y1 = (dist - origin * np.cos(angle)) / np.sin(angle)
         ax.plot(origin, (y0, y1), '-r')
-    plt.show(block=False)
-    plt.close()
-    fig.savefig("hough.png")
-    # fig.canvas.draw()
-    # data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
-    # data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-    # imgplot = plt.imshow(data,cmap=plt.get_cmap('gray'))
-    # plt.show()
 
-# plt.show()
-    # acc,theta_peak,rho_peak =extract_lines(accs,ths,rs,threshold)
-
-    # return acc,theta_peak,rho_peak
-    return mpimg.imread('hough.png')
-
-# img_c = mpimg.imread('images\chess_board.jpg')
-# get_hough_lines(img_c)
-
-
-
-
-# def hough_line(img, angle_step=1, lines_are_white=True, value_threshold=5):
-    
-#     img = cv2.Canny(img,100,200)
-#     # rho and theta ranges
-#     thetas = np.deg2rad(np.arange(-90.0, 90.0, angle_step))
-#     width, height = img.shape
-#     diag_len = int(round(math.sqrt(width * width + height * height)))
-#     rhos = np.linspace(-diag_len, diag_len, diag_len * 2)
-
-#     cos_t = np.cos(thetas)
-#     sin_t = np.sin(thetas)
-#     num_thetas = len(thetas)
-
-#     # Hough accumulator 
-#     accumulator = np.zeros((2 * diag_len, num_thetas), dtype=np.uint8)
-#     are_edges = img > value_threshold if lines_are_white else img < value_threshold
-#     y_idxs, x_idxs = np.nonzero(are_edges)
-
-#     # hough accumulator voting
-#     for i in range(len(x_idxs)):
-#         x = x_idxs[i]
-#         y = y_idxs[i]
-
-#         for t_idx in range(num_thetas):
-#             rho = diag_len + int(round(x * cos_t[t_idx] + y * sin_t[t_idx]))
-#             accumulator[rho, t_idx] += 1
-
-#     return accumulator, thetas, rhos
-
-
-# def show_hough_line(img, accumulator, thetas, rhos, save_path=None):
-#     import matplotlib.pyplot as plt
-
-#     fig, ax = plt.subplots(1, 2, figsize=(10, 10))
-
-#     ax[0].imshow(img, cmap=plt.cm.gray)
-#     ax[0].set_title('Input image')
-#     ax[0].axis('image')
-
-#     ax[1].imshow(
-#         accumulator, cmap='jet',
-#         extent=[np.rad2deg(thetas[-1]), np.rad2deg(thetas[0]), rhos[-1], rhos[0]])
-#     ax[1].set_aspect('equal', adjustable='box')
-#     ax[1].set_title('Hough transform')
-#     ax[1].set_xlabel('Angles (degrees)')
-#     ax[1].set_ylabel('Distance (pixels)')
-#     ax[1].axis('image')
-
-#     if save_path is not None:
-#         plt.savefig(save_path, bbox_inches='tight')
-#     plt.show()
-
-
-# if __name__ == '__main__':
-#     # pass here the image path 
-#     imgpath = '/home/muhamed/cv404-2020-assignment-02-sbme404-2020-team05/images/dog.jpg'
-#     img = imageio.imread(imgpath)
-#     if img.ndim == 3:
-#         img = rgb2gray(img)
-#     accumulator, thetas, rhos = hough_line(img)
-#     # pass here the saved path
-#     show_hough_line(img, accumulator, save_path='/home/muhamed/cv404-2020-assignment-02-sbme404-2020-team05/output01.jpg')
+    fig.canvas.draw()
+    out = np.array(fig.canvas.renderer.buffer_rgba())
+    plt.close('all') 
+    return out 
 
 from PIL import Image, ImageDraw
 from math import sqrt, pi, cos, sin
